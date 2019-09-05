abc4ddd9d798b3638daa1a7b146c4f6ddfe42d17
Alan Gabbianelli <alan.gabbianelli@digital.cabinet-office.gov.uk>
2019-09-05 10:48:26 +0100

    Add duration option to current simulations
    This gives current simulations the ability to be run with a specified
    `duration`.
    
    Co-Authored-By: Mahmud Hussain <mahmud.hussain@digital.cabinet-office.gov.uk>

--- src/test/scala/govuk/DynamicListsEmailSignup.scala
+++ src/test/scala/govuk/DynamicListsEmailSignup.scala
@@ -6,6 +6,8 @@ import io.gatling.http.Predef._
 class DynamicListsEmailSignup extends Simulation {
   val factor = sys.props.getOrElse("factor", "1").toFloat
 
+  val duration = sys.props.getOrElse("duration", "0").toInt
+
   val paths = csv(dataDir + java.io.File.separatorChar + "get-ready-brexit-check-email-signup_paths.csv").readRecords
 
   val scale = factor / workers
@@ -16,10 +18,27 @@ class DynamicListsEmailSignup extends Simulation {
       .foreach(paths, "path") {
         exec(flattenMapIntoAttributes("${path}"))
           .repeat(session => math.ceil(session("hits").as[Int] * scale).toInt,
-            "hit") {
+                  "hit") {
             exec(get("${base_path}", "${cachebust}-${hit}"))
           }
       }
 
-  run(scn)
+  val scn_with_duration =
+    scenario("DynamicListsEmailSignup")
+      .during(duration, "Soak test"){
+        feed(cachebuster)
+        .foreach(paths, "path") {
+          exec(flattenMapIntoAttributes("${path}"))
+            .repeat(session => math.ceil(session("hits").as[Int] * scale).toInt,
+                    "hit") {
+              exec(get("${base_path}", "${cachebust}-${hit}"))
+            }
+        }
+      }
+
+  if(duration > 0){
+    run(scn_with_duration)
+  } else{
+    run(scn)
+  }
 }
